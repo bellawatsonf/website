5d8b224f7412f6effb434c1d596bd172c12324d8
bellawatsonf <88996204+bellawatsonf@users.noreply.github.com>
2025-01-20 11:40:57 +0700

    grid

--- src/components/OurClient.tsx
+++ src/components/OurClient.tsx
@@ -5,13 +5,13 @@ export default function OurClientComponent(params) {
   return (
     <>
       <div className="py-[24px] md:py-[80px] px-[100px]">
-        <p className="text-[#0056B3] font-[Poppins] text-[32px] md:text-[40px] font-semibold leading-[32px] text-center pb-[60px]">
+        <p className="text-[#0056B3] font-[Poppins] text-[32px] md:text-[40px] font-semibold leading-[32px] text-center pb-[30px]">
           Our Clients
         </p>
         <div className={`grid grid-cols-1 md:grid-cols-3 gap-4`}>
           {params?.portoClient?.map((el) => (
-            <div className="flex items-center py-[15px]">
-              <img src={el.url_image} className="block m-auto w-[300px]" />
+            <div className="flex items-center pb-[15px]">
+              <img src={el.url_image} className="block m-auto w-[200px]" />
             </div>
           ))}
         </div>
